5f4fb350c15f88bad1144a9e9a3eb6b4f656393c
unknown <envicomm.ondong@gmail.com>
2024-01-04 14:33:27 -0500

    first lesson

---
+++ index.js
@@ -0,0 +1,17 @@
+// 7 datatypes in JS (strings, integers, float, objects, arrays, booleans, null)
+//camel case for variables
+//const, let, var (declaring of var)
+// + sign in strings combines two values of a variable while in integers/float the sign is the operator
+
+
+
+
+let name = "5.3"
+let age = "6"
+
+console.log(name-age)
+
+
+
+
+
